e235e3a7d174f566f553f991287d36c151e29fd1
zoheirkhonyagar <zoheirkhonyagar@gmail.com>
2023-05-07 16:48:39 +0330

    feat: added and cofigured application colors in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,44 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
-    },
+      textColor: {
+        skin: {
+          'color-one': 'rgba(var(--color-one), <alpha-value>)',
+          'color-two': 'rgba(var(--color-two), <alpha-value>)',
+          'color-three': 'rgba(var(--color-three), <alpha-value>)',
+          'color-four': 'rgba(var(--color-four), <alpha-value>)',
+          'color-five': 'rgba(var(--color-five), <alpha-value>)',
+          'color-six': 'rgba(var(--color-six), <alpha-value>)',
+          'color-stroke': 'rgba(var(--color-stroke), <alpha-value>)',
+          'color-protein': 'rgba(var(--color-protein), <alpha-value>)',
+          'color-fat': 'rgba(var(--color-fat), <alpha-value>)',
+          'color-carb': 'rgba(var(--color-carb), <alpha-value>)'
+        }
+      },
+      backgroundColor: {
+        skin: {
+          'color-one': 'rgba(var(--color-one), <alpha-value>)',
+          'color-two': 'rgba(var(--color-two), <alpha-value>)',
+          'color-three': 'rgba(var(--color-three), <alpha-value>)',
+          'color-four': 'rgba(var(--color-four), <alpha-value>)',
+          'color-five': 'rgba(var(--color-five), <alpha-value>)',
+          'color-six': 'rgba(var(--color-six), <alpha-value>)',
+          'color-stroke': 'rgba(var(--color-stroke), <alpha-value>)',
+          'color-protein': 'rgba(var(--color-protein), <alpha-value>)',
+          'color-fat': 'rgba(var(--color-fat), <alpha-value>)',
+          'color-carb': 'rgba(var(--color-carb), <alpha-value>)'
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }

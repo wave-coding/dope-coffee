fcd89718e6561e41eb48642cce8b0e9a66ee1b4e
zoheirkhonyagar <zoheirkhonyagar@gmail.com>
2023-05-09 12:09:39 +0330

    feat: finalizing colors settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,33 +12,17 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
-      textColor: {
-        skin: {
-          'color-one': 'rgba(var(--color-one), <alpha-value>)',
-          'color-two': 'rgba(var(--color-two), <alpha-value>)',
-          'color-three': 'rgba(var(--color-three), <alpha-value>)',
-          'color-four': 'rgba(var(--color-four), <alpha-value>)',
-          'color-five': 'rgba(var(--color-five), <alpha-value>)',
-          'color-six': 'rgba(var(--color-six), <alpha-value>)',
-          'color-stroke': 'rgba(var(--color-stroke), <alpha-value>)',
-          'color-protein': 'rgba(var(--color-protein), <alpha-value>)',
-          'color-fat': 'rgba(var(--color-fat), <alpha-value>)',
-          'color-carb': 'rgba(var(--color-carb), <alpha-value>)'
-        }
-      },
-      backgroundColor: {
-        skin: {
-          'color-one': 'rgba(var(--color-one), <alpha-value>)',
-          'color-two': 'rgba(var(--color-two), <alpha-value>)',
-          'color-three': 'rgba(var(--color-three), <alpha-value>)',
-          'color-four': 'rgba(var(--color-four), <alpha-value>)',
-          'color-five': 'rgba(var(--color-five), <alpha-value>)',
-          'color-six': 'rgba(var(--color-six), <alpha-value>)',
-          'color-stroke': 'rgba(var(--color-stroke), <alpha-value>)',
-          'color-protein': 'rgba(var(--color-protein), <alpha-value>)',
-          'color-fat': 'rgba(var(--color-fat), <alpha-value>)',
-          'color-carb': 'rgba(var(--color-carb), <alpha-value>)'
-        }
+      colors: {
+        'color-one': 'rgba(var(--color-one), <alpha-value>)',
+        'color-two': 'rgba(var(--color-two), <alpha-value>)',
+        'color-three': 'rgba(var(--color-three), <alpha-value>)',
+        'color-four': 'rgba(var(--color-four), <alpha-value>)',
+        'color-five': 'rgba(var(--color-five), <alpha-value>)',
+        'color-six': 'rgba(var(--color-six), <alpha-value>)',
+        'color-stroke': 'rgba(var(--color-stroke), <alpha-value>)',
+        'color-protein': 'rgba(var(--color-protein), <alpha-value>)',
+        'color-fat': 'rgba(var(--color-fat), <alpha-value>)',
+        'color-carb': 'rgba(var(--color-carb), <alpha-value>)'
       }
     }
   },

b5ead70cfcab0c64211d19da9277a20966c130ae
zoheirkhonyagar <zoheirkhonyagar@gmail.com>
2023-05-28 18:12:41 +0330

    fix: fixed some style issues and refactored the whole component

--- components/Chip/index.tsx
+++ components/Chip/index.tsx
@@ -5,12 +5,10 @@ import Icons from '@/components/Icon';
 
 export interface ChipProps {
   type: ChipType;
-  intensity?: IntensityType;
+  intensity?: IntensityType | null;
 }
 const intensitySelector: ChipFunc = (intensity = null) => {
-  if (!intensity) return null;
-
-  return Icons[intensity];
+  return intensity ? Icons[intensity] : null;
 };
 
 export const Chips: ChipRecords = {
@@ -21,15 +19,31 @@ export const Chips: ChipRecords = {
   Vegetarian: () => null,
 };
 
+const colorVariants = {
+  Protein: 'bg-color-protein',
+  Fat: 'bg-color-fat',
+  Carb: 'bg-color-carb',
+  Vegetarian: 'bg-color-vegetarian',
+  Hug: 'bg-color-hug',
+};
+
+const getIconByChipType = ({
+  type,
+  intensity = null,
+}: ChipProps): Icon | null => {
+  return typeof intensity === 'string' ? Chips?.[type]?.(intensity) : null;
+};
+
 const Chip: FC<ChipProps> = ({ type, intensity = null }) => {
-  const Icon: Icon | null =
-    typeof intensity === 'string' ? Chips[type](intensity) : null;
+  const Icon: Icon | null = getIconByChipType({ type, intensity });
+
+  const buttonClass = `${colorVariants[type]} inline-flex items-center rounded-md px-1 text-white`;
+  const spanClass = `${type !== 'Vegetarian' ? 'ml-1' : ''}  text-label-sm`;
 
   return (
-    <button
-      className={` flex content-center items-center justify-center space-x-1.5 rounded-md px-1`}
-    >
-      {Icon && <Icon className="h-3 w-3" />}
+    <button className={buttonClass}>
+      {Icon && <Icon fill="white" className="h-3 w-3 align-middle" />}
+      {type === 'Hug' ? null : <span className={spanClass}>{type}</span>}
     </button>
   );
 };

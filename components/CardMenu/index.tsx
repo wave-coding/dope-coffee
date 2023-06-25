43ac872b52c09310661ff95db8a745ef3fb19a2e
zoheirkhonyagar <zoheirkhonyagar@gmail.com>
2023-06-25 16:17:18 +0330

    fix: chnage Icon position and sizing to be more flexible

---
+++ components/CardMenu/index.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+
+interface CardMenuProps {
+  title: string;
+  items: string;
+  frames: string[];
+  price: number;
+  quantity: number;
+  calorie: number;
+}
+
+const CardMenu: FC<CardMenuProps> = () => <></>;
+
+export default CardMenu;
--- components/CardRestaurantLocation/index.tsx
+++ components/CardRestaurantLocation/index.tsx
@@ -19,7 +19,7 @@ interface StarsProps {
 const Stars: FC<StarsProps> = ({ count = 5, score }) => {
   const Icon = Icons['filled-star'];
   return (
-    <div className="stars flex">
+    <div className="flex w-full items-center">
       {Array.from({ length: count }, (_, index) => {
         const scoreMinusIndex = score - index;
         if (scoreMinusIndex > 0 && scoreMinusIndex < 1) {
@@ -28,22 +28,25 @@ const Stars: FC<StarsProps> = ({ count = 5, score }) => {
               className="fill-color-star stroke-color-star"
               key={index}
               strokeWidth={1}
-              width={24}
-              height={24}
-              endPercentage={scoreMinusIndex * 100}
-              linearGradientStopColor="#FFAE5F"
-              linearGradientLastStopColor="transparent"
+              // width={24}
+              // height={24}
+              viewBox="-4 -2 24 24"
+              end-percentage={scoreMinusIndex * 100}
+              stopColor="#FFAE5F"
+              linear-gradient-last-stop-color="transparent"
+              linear-gradient-stop-color="#FFAE5F"
             />
           );
         } else {
           return (
             <Icon
+              viewBox="-4 -2 24 24"
               key={index}
               fill="#FFAE5F"
               stroke="#FFAE5F"
               strokeWidth={1.3}
-              width={24}
-              height={24}
+              // width={24}
+              // height={24}
             />
           );
         }
@@ -62,16 +65,22 @@ const CardRestaurantLocation: FC<CardRestaurantLocationProps> = ({
   const Icon = Icons['filled-star'];
   return (
     <div className="flex w-auto flex-col overflow-hidden rounded-md bg-color-three p-2 pl-3 text-white">
-      <div className="title flex">
+      <div className="title flex items-center justify-between">
         <h2 className="mb-1 mt-1 text-headline-lg">{name}</h2>
-        <div className="flex w-1/6 flex-row items-center">
-          <Icon fill="transparent" className="flex stroke-white" />
+        <div className="flex w-1/6">
+          <Icon
+            viewBox="-4 -4 24 24"
+            fill="transparent"
+            className="stroke-white"
+          />
         </div>
       </div>
       <h3 className="mb-1 mt-1 text-headline-md">{type}</h3>
-      <div className="flex items-center">
-        <Stars score={score} />
-        <div>
+      <div className="flex items-center gap-1">
+        <div className="stars w-1/3">
+          <Stars score={score} />
+        </div>
+        <div className="w-2/3">
           {score} ({reviewsCount} Reviews)
         </div>
       </div>

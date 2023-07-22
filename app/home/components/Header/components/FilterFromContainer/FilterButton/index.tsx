3bd2f7569a2e2fb4e690104c92de572b4c66219d
zoheirkhonyagar <zoheirkhonyagar@gmail.com>
2023-07-22 14:10:18 +0330

    feat: added nested components

---
+++ app/home/components/Header/components/FilterFromContainer/FilterButton/index.tsx
@@ -0,0 +1,19 @@
+import { Button } from '@/components';
+
+const FilterButton = () => {
+  return (
+    <div className="flex w-2/12">
+      <div className="h-full w-full">
+        <Button
+          type="primary"
+          iconProps={{ viewBox: '2 -2 16 24' }}
+          iconName="filter"
+          text="Filter"
+          classNames="justify-evenly"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FilterButton;
